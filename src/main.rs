0ec7b10bc2dd0d81cf7725268ff46c6dfdb141d3
Sumit <sumitjcode@gmail.com>
2025-02-27 23:53:20 +0530

    feat: add initial project structure and dependencies.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod fetcher;
+mod frontier;
+mod parser;
+mod politeness;
+mod storage;
+
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    println!("Starting web crawler...");
+
+    Ok(())
 }
